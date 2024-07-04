c09f1aa14b73d28ce43f9e69a1a00ce85bccfdc1
Ajinkya Sonar <ajinkya.sonar@sportzinteractive.net>
2024-07-04 15:44:33 +0530

    Version 0.0.4 on board

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "CustomFramework",
+    platforms: [
+        .macOS(.v12), .iOS(.v15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -12,12 +15,10 @@ let package = Package(
             targets: ["CustomFramework"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "CustomFramework"),
-        .testTarget(
-            name: "CustomFrameworkTests",
-            dependencies: ["CustomFramework"]),
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(
+            name: "CustomFramework",
+            path: "./Sources/CustomFramework.xcframework")
     ]
 )
