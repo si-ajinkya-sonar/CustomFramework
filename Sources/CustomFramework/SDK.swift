b3ad81ee7d73c8c7af0cacc2bbc2c28d80e66d2e
Ajinkya Sonar <ajinkya.sonar@sportzinteractive.net>
2024-07-04 15:13:44 +0530

    Initial framework files

---
+++ Sources/CustomFramework/SDK.swift
@@ -0,0 +1,14 @@
+//
+//  SDK.swift
+//  CustomFrameworkLesson
+//
+//  Created by Nausheen Khan on 04/07/24.
+//
+
+import Foundation
+
+struct SDK {
+    static func doSomeWork() {
+        print("Doing Some Work...")
+    }
+}
